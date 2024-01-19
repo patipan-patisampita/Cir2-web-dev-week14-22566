6b8514ae1fc8b8602cb4f6a557e0394f80bec680
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-19 14:36:41 +0700

    update

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php include('includes/header.php'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            Admin Dashboard
+        </div>
+    </div>
+</div>
+<?php include('includes/footer.php'); ?>
